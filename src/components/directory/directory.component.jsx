31eb480a3c279dd85bb1d66ad4a9ac8944369f57
unknown <gurmeet.ece.111040@gmail.com>
2019-07-20 16:39:52 +0530

    Prettified the files

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
-import './directory.styles.scss';
+import "./directory.styles.scss";
 
 class Directory extends Component {
   constructor() {
@@ -45,7 +45,7 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id,size }) => (
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
           <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
