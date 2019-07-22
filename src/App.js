9c52985ae90620462f2734b7bfb9b53162e21b74
unknown <gurmeet.ece.111040@gmail.com>
2019-07-22 23:36:55 +0530

    router added in the project to navigate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Homepage } from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        {" "}
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
